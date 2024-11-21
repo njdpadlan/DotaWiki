bc314b209e3ad497158ae4069ff7d6927efb3a8f
njdpadlan <169112404+njdpadlan@users.noreply.github.com>
2024-11-20 22:40:02 -0700

    updated bootstrap and added functionalities

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 # DotaWiki
-Dota 2 wiki
+Welcome Dota 2 Wiki
 
 Hi! I'm Nicole, an aspiring web developer. 
 
-I'm currently doing this as a personal project of mine since I'm very passionate about Dota 2.
+I'm currently doing this as a personal project of mine since I'm passionate about Dota 2.
+
+Huge thanks to OpenDota API (25.0.0) for creating this Dota API
+Link: https://docs.opendota.com/
 
 
-Milestones: 
+Progress: 
 **November 19, 2024**
 1. Render all heroes with hero_images and hero_names
 2. Sort list of heroes alphabetically.
+
+**November 20, 2024**
+1. Initialize NPM (npm init)
+2. Added a seperate js file for API fetch requests
+3. Updated URL to add Dawn Breaker (previously her img is broken)
+4. Updated html and bootstrap to add cards and nav buttons
+5. Fixed cards placement (flex, margin, etc..)
+6. Added search function
+7. Added sort function by Attribute (Str, Agi, Int, Universal)
--- js/main.js
+++ js/main.js
@@ -1,39 +1,116 @@
+//Imports the API fetch requests
 import { getDotaHero } from "./api.js";
 
-let herolist = [];
+let heroList = [];
+let filteredHeroList = [];
 
-getDotaHero().then(dotaheroesArray => {
-    sortHeroListAlphabetically(dotaheroesArray);
-    renderHeroList(dotaheroesArray)
-})
+let heroesFormElement = document.querySelector(".form-inline");
+let herolistElement = document.querySelector(".hero-list");
+let strFilter = document.querySelector(".btn-str");
+let agiFilter = document.querySelector(".btn-agi");
+let intFilter = document.querySelector(".btn-int");
+let univFilter = document.querySelector(".btn-all");
 
-//this will sort heroes alphabetically
+
+getDotaHero().then((dotaheroesArray) => {
+  sortHeroListAlphabetically(dotaheroesArray);
+  renderHeroList(dotaheroesArray);
+  heroList = dotaheroesArray;
+});
+
+//Sort heroes alphabetically
 const sortHeroListAlphabetically = (heroesArray) => {
-    heroesArray.sort((firstHeroName, secondHeroName) => {
-        if (firstHeroName.localized_name < secondHeroName.localized_name) {
-            return -1;
-        }
-    })
-}
+  heroesArray.sort((firstHeroName, secondHeroName) => {
+    if (firstHeroName.localized_name < secondHeroName.localized_name) {
+      return -1;
+    }
+  });
+};
 
-const renderHeroList = (testherodetails) => {
+//renders all the heroes
+const renderHeroList = (dotaheroes) => {
 
-    testherodetails.forEach(herodetails => {
+  herolistElement.innerHTML = "";
+
+  dotaheroes.forEach((herodetails) => {
     //removes the "npc_dota_hero_" and gets the hero name
     let heroName = herodetails.name.replace("npc_dota_hero_", "");
+
     //URL from Cloudflare for hosting Dota 2 assets
     let imgURL = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${heroName}.png`;
-    let herolistElement = document.querySelector(".hero-list")
-    let herocards = `<div class="col">
-        <div class="card shadow-sm">
-          <img class="bd-placeholder-img card-img-top" src="${imgURL}"/>
-          <div class="card-body">
-            <h5 class="card-title">${herodetails.localized_name}</h5>
-          </div>
+
+    let heroCards = `<div class="card">
+        <img class="card-img-top" src="${imgURL}">
+        <div class="card-body">
+          <h5 class="card-title">${herodetails.localized_name}</h5>
         </div>
-      </div>`  
-      herolistElement.innerHTML += herocards; 
-    })
+      </div>`;
+    herolistElement.innerHTML += heroCards;
+  });
+};
+
+//search a hero function
+const searchHero = (heroSearch, heroList) => {
+let lowercaseFilter = heroSearch.toLowerCase();
+
+let filteredHeroes = heroList.filter(heroes => {
+  return heroes.localized_name.toLowerCase().includes(lowercaseFilter)
+})
+return filteredHeroes;
+}
+
+//search a hero event listener
+heroesFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+ 
+  filteredHeroList = heroList
+
+  let searchHeroElement = event.target.elements["search-hero"];
+
+  if (searchHeroElement.value != "") {
+    filteredHeroList = searchHero(searchHeroElement.value, filteredHeroList);
+  }
+  renderHeroList(filteredHeroList)
+})
+
+//sort heroes by STR
+strFilter.addEventListener("click", () => {
+  let strValue = strFilter.value
+  filteredHeroList = heroList
+  filteredHeroList = sortByATTR(strValue, filteredHeroList)
+  renderHeroList(filteredHeroList)
+})
+
+//sort heroes by AGI
+agiFilter.addEventListener("click", () => {
+  let agiValue = agiFilter.value
+  filteredHeroList = heroList
+  filteredHeroList = sortByATTR(agiValue, filteredHeroList)
+  renderHeroList(filteredHeroList)
+})
+
+//sort heroes by INT
+intFilter.addEventListener("click", () => {
+  let intValue = intFilter.value
+  filteredHeroList = heroList
+  filteredHeroList = sortByATTR(intValue, filteredHeroList)
+  renderHeroList(filteredHeroList)
+})
+
+//sort heroes by UNIVERSAL
+univFilter.addEventListener("click", () => {
+  let univValue = univFilter.value
+  filteredHeroList = heroList
+  filteredHeroList = sortByATTR(univValue, filteredHeroList)
+  renderHeroList(filteredHeroList)
+})
+
+//filters the list by ATTR (str, agi, int, all/universal)
+const sortByATTR = (heroAttr, heroList) => {
+  let attrHeroes = heroList.filter(hero => {
+    return hero.primary_attr === heroAttr;
+  })
+  return attrHeroes
 }
 
 
